c6dcabbd949b2fe20486fcba18a05acacbc07772
“João <eng.joao.pedro@outlook.com>
2022-10-10 22:54:57 -0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,40 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+import "./index.css";
+
+function BookList() {
+  return (
+    <section className="booklist">
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+    </section>
+  );
+}
+
+const Book = () => {
+  return (
+    <article className="book">
+      <Image />
+      <Title />
+      <Author />
+    </article>
+  );
+};
+
+const Image = () => (
+  <img
+    src="https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T1/images/I/51bZujlJxlL._SX218_BO1,204,203,200_QL40_FMwebp_.jpg"
+    alt="Harry Potter"
+  ></img>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const Title = () => <h1>Harry Potter and the Order of the Phoenix</h1>;
+
+const Author = () => <h4>J. K. Rowling</h4>;
+
+ReactDOM.render(<BookList />, document.getElementById("root"));
