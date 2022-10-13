8e86fa54b605e86015a02a81e44e79c0aa796d18
“João <eng.joao.pedro@outlook.com>
2022-10-13 13:00:46 -0300

    refactor: import data books and map

--- src/index.js
+++ src/index.js
@@ -3,38 +3,17 @@ import ReactDOM from "react-dom";
 
 import "./index.css";
 
+import Book from "./components/Book";
+import { books } from "./books";
+
 function BookList() {
   return (
     <section className="booklist">
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
+      {books.map((book) => {
+        return <Book key={book.id} {...book} />;
+      })}
     </section>
   );
 }
-
-const Book = () => {
-  return (
-    <article className="book">
-      <Image />
-      <Title />
-      <Author />
-    </article>
-  );
-};
-
-const Image = () => (
-  <img
-    src="https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T1/images/I/51bZujlJxlL._SX218_BO1,204,203,200_QL40_FMwebp_.jpg"
-    alt="Harry Potter"
-  ></img>
-);
-
-const Title = () => <h1>Harry Potter and the Order of the Phoenix</h1>;
-
-const Author = () => <h4>J. K. Rowling</h4>;
 
 ReactDOM.render(<BookList />, document.getElementById("root"));
