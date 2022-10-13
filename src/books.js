70d372d6a6a4cdaae1056e85361944e648ae4b77
“João <eng.joao.pedro@outlook.com>
2022-10-13 12:59:42 -0300

    refactor: books database

---
+++ src/books.js
@@ -0,0 +1,20 @@
+export const books = [
+  {
+    id: 1,
+    img: "https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T1/images/I/51bZujlJxlL._SX218_BO1,204,203,200_QL40_FMwebp_.jpg",
+    title: "Harry Potter and the Order of the Phoenix",
+    author: "J. K. Rowling",
+  },
+  {
+    id: 2,
+    img: "https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T1/images/I/51um9zycLwL._SX396_BO1,204,203,200_.jpg",
+    title: "The Complete Cookbook for Young Chefs",
+    author: "Americas Test Kitchen Kids",
+  },
+  {
+    id: 3,
+    img: "https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T1/images/I/41d1gVUK1yL._SY291_BO1,204,203,200_QL40_FMwebp_.jpg",
+    title: "Verity",
+    author: "Colleen Hoover",
+  },
+];
